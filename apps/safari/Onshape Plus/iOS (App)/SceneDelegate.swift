c9e506f42fee4cbf105c07fabd9117273dc27df2
RileyDavidson-Evans <riley.k.davidson@gmail.com>
2026-06-05 11:17:43 -0400

    Add Onshape Plus Safari extension for iOS and macOS
    - Implemented main functionality in ViewController.swift to handle web view and extension state.
    - Created SafariWebExtensionHandler.swift to manage extension requests.
    - Added user interface files including LaunchScreen.storyboard and Main.storyboard for iOS.
    - Developed corresponding AppDelegate and SceneDelegate for iOS app lifecycle management.
    - Designed macOS app structure with AppDelegate and Main.storyboard for user interface.
    - Included CSS and JavaScript resources for styling and functionality in the extension.
    - Configured Info.plist files for both iOS and macOS apps and extensions.

---
+++ apps/safari/Onshape Plus/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Riley Davidson-Evans on 2026-06-05.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
--- public/background.js
+++
@@ -1,5 +0,0 @@
-chrome.commands.onCommand.addListener((command) => {
-  if (command === "reload-extension") {
-    chrome.runtime.reload();
-  }
-});
